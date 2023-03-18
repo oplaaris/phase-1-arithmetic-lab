954435208e4fab06d1f4ba7d6cadba9ac8522d9a
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-03-18 13:16:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); // Output: 62
+
+
+// Create a variable called random that will generate a random integer greater than 0.
+let random = Math.ceil(Math.random() * 100);
+console.log(random); // Output: a random integer between 1 and 100
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod); // Output: 4
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+let numbers = [5, 10, 15, 20, 1];
+let max = Math.max(...numbers);
+console.log(max); // Output: 100
